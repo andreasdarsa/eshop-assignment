273cd2768bac3673db2b3fddc61db18db83840d9
Vasileios Zervos <hadesthefox888@gmail.com>
2025-04-20 19:29:07 +0300

    - Finished products.html and nav buttons on homepage and products

--- products.js
+++ products.js
@@ -1,34 +1,63 @@
 let i = 0;
 let intervalID;
 const slides = document.getElementsByClassName('slide');
+const slideshow = document.querySelector('.slideshow');
 const nextButton = document.querySelector('.next');
 const prevButton = document.querySelector('.prev');
+const homeButton = document.getElementById('home');
+const searchButton = document.getElementById('search');
+const productsButton = document.getElementById('toProducts');
+const aboutButton = document.getElementById('about');
 
-function showNextSlide() {
-    slides[i].classList.remove('active');
-    i = (i + 1) % slides.length;
-    slides[i].classList.add('active');
-}
+if (slideshow) {
+    function showNextSlide() {
+        slides[i].classList.remove('active');
+        i = (i + 1) % slides.length;
+        slides[i].classList.add('active');
+    }
 
-function showPrevSlide() {
-    slides[i].classList.remove('active');
-    i = (i - 1 + slides.length) % slides.length;
-    slides[i].classList.add('active');
-}
+    function showPrevSlide() {
+        slides[i].classList.remove('active');
+        i = (i - 1 + slides.length) % slides.length;
+        slides[i].classList.add('active');
+    }
 
-function restartInterval() {
-    clearInterval(intervalID);
+    function restartInterval() {
+        clearInterval(intervalID);
+        intervalID = setInterval(showNextSlide, 3000);
+    }
+
+    nextButton.addEventListener('click', () => {
+        showNextSlide();
+        restartInterval();
+    });
+
+    prevButton.addEventListener('click', () => {
+        showPrevSlide();
+        restartInterval();
+    });
+
     intervalID = setInterval(showNextSlide, 3000);
 }
 
-nextButton.addEventListener('click',() => {
-    showNextSlide();
-    restartInterval();
-});
-
-prevButton.addEventListener('click',() => {
-    showPrevSlide();
-    restartInterval();
-});
-
-intervalID= setInterval(showNextSlide, 3000);
+if (homeButton) {
+    homeButton.addEventListener('click', () => {
+         window.location.href = 'homepage.html';
+    });
+}
+if (searchButton) {
+    searchButton.addEventListener('click', () => {
+        const searchBar = document.getElementById('searchBar');
+        searchBar.classList.toggle('show');
+    });
+}
+if (productsButton) {
+    productsButton.addEventListener('click', () => {
+        window.location.href = 'products.html';
+    });
+}
+if (aboutButton) {
+    aboutButton.addEventListener('click', () => {
+         alert('name1 - AEM1\nname2 - AEM2\nname3 - AEM3\n');
+    });
+}
